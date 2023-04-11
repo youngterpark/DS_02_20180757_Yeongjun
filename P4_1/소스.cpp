c021d8a18663857044668fe80c168877fbeff8f2
youngterpark <87702751+youngterpark@users.noreply.github.com>
2023-04-11 09:37:04 +0900

    Update 소스.cpp

--- P4_1/소스.cpp
+++ P4_1/소스.cpp
@@ -5,16 +5,17 @@
 using namespace std;
 
 int main(void) {
+	// 하나씩 실행해야 함.
 	// 4.1.1
-	//Node* head = NULL; //head노드 NULL
-	//
-	//// 원형 연결리스트 구성
-	//Append_Node(&head, Create_Node(15));
-	//Append_Node(&head, Create_Node(25));
-	//Append_Node(&head, Create_Node(31));
-	//Append_Node(&head, Create_Node(24));
-	//// 원형 연결리스트 출력
-	//Print_Circular_Linked_List(head);
+	Node* head = NULL; //head노드 NULL
+	
+	// 원형 연결리스트 구성
+	Append_Node(&head, Create_Node(15));
+	Append_Node(&head, Create_Node(25));
+	Append_Node(&head, Create_Node(31));
+	Append_Node(&head, Create_Node(24));
+	// 원형 연결리스트 출력
+	Print_Circular_Linked_List(head);
 
 	// 4.1.2
 	Node* tail = NULL;
