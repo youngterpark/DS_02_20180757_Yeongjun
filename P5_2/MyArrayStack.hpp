6011460d1e370473faea6321a33aca900f567641
youngterpark <87702751+youngterpark@users.noreply.github.com>
2023-04-27 08:27:25 +0900

    Update MyArrayStack.hpp

--- P5_2/MyArrayStack.hpp
+++ P5_2/MyArrayStack.hpp
@@ -4,25 +4,25 @@
 #include <cstdlib>
 #define STACK_LEN 100
 template <typename T> // 템플릿: 다양한 유형의 데이터에서 작동할 수 있는 함수와 클래스를 작성할 수 있음
-class ArrStack {
-private:
-	T* arr;
-	int length;
-	int top;
-public:
-	ArrStack() {
-		top = -1;
-		length = STACK_LEN;
-		arr = new T[STACK_LEN];
+class ArrStack { // 배열 스택 클래스 정의
+private: // 클래스에서만 쓰는 변수
+	T* arr; // 다양한 유형의 데이터(정수, 문자열)을 가지는 배열
+	int length; // 배열 길이
+	int top; // top 인덱스
+public: // public
+	ArrStack() { //길이 지정안하면
+		top = -1; // top은 -1(스택 초기값)
+		length = STACK_LEN; // STACK_LEN 기본 100 (#define)
+		arr = new T[STACK_LEN]; // arr 초기화
 	}
-	ArrStack(int len) {
+	ArrStack(int len) { //길이 따로 지정하면
 		top = -1;
-		length = len;
+		length = len; // 그 길이로
 		arr = new T[len];
 	}
 	void Stack_Init() // 배열 스택 초기화
 	{
-		arr[top] = -1;
+		arr[top] = -1; //
 	}
 	void Stack_Push(T item) // 데이터 삽입
 	{
