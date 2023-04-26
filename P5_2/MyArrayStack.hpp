307574785a89b2aaf473f8bb0f402896a324e6cc
youngterpark <87702751+youngterpark@users.noreply.github.com>
2023-04-27 08:45:23 +0900

    Update MyArrayStack.hpp

--- P5_2/MyArrayStack.hpp
+++ P5_2/MyArrayStack.hpp
@@ -1,1 +1,68 @@
+#ifndef __MY__ARRAY_STACK_HPP__
+#define __MY__ARRAY_STACK_HPP__
+#include <cstdio>
+#include <cstdlib>
+#define STACK_LEN 100
+template <typename T> // 템플릿: 다양한 유형의 데이터에서 작동할 수 있는 함수와 클래스를 작성할 수 있음
+class ArrStack {
+private:
+	T* arr; // 다양한 데이터(정수, 문자열) 받을 수 있는 배열;
+	int length; // 배열 길이
+	int top; // top 인덱스
+public:
+	ArrStack() {
+		top = -1; // top 초기값 -1
+		length = STACK_LEN;
+		arr = new T[STACK_LEN];
+	}
+	ArrStack(int len) {
+		top = -1;
+		length = len;
+		arr = new T[len];
+	}
+	void Stack_Init() // 배열 스택 초기화
+	{
+		for (int i = 0; i < STACK_LEN; i++) {
+			arr[i] = 0;
+		}
+	}
+	void Stack_Push(T item) // 데이터 삽입
+	{
+		if (!Stack_IsFull()) { // 가득차 있지 않다면,
 
+			top++;
+			arr[top] = item;
+		}
+		else {
+			printf("Stack is full!\n");
+			exit(1);
+		}
+	}
+	T Stack_Pop() // 데이터 인출
+	{
+		int idx;
+		if (Stack_IsEmpty()){ //비어있다면,
+			printf("Stack is empty!\n");
+			exit(1);
+		}
+		else {
+			idx = top;
+			top--;
+			return arr[idx];
+		}
+	}
+	T Stack_Peek() // 최상단 데이터 확인
+	{
+		return arr[top];
+	}
+	bool Stack_IsEmpty() // 스택이 비어있는지 확인
+	{
+		return top == -1;
+	}
+	bool Stack_IsFull() // 스택이 가득 차 있는지 확인
+	{
+		return top == length-1;
+	}
+}; //Array Stack 클래스 선언
+
+#endif // ! __MY__ARRAY_STACK_HPP__
