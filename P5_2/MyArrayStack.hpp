839fdf6d6bfe4106b7c0843eaca61f00c4e8d0e9
youngterpark <87702751+youngterpark@users.noreply.github.com>
2023-04-27 15:22:00 +0900

    Update MyArrayStack.hpp

--- P5_2/MyArrayStack.hpp
+++ P5_2/MyArrayStack.hpp
@@ -10,58 +10,58 @@ private:
 	int length; // 배열 길이
 	int top; // top 인덱스
 public:
-	ArrStack() {
+	ArrStack() { // 길이 따로 지정 안하면
 		top = -1; // top 초기값 -1
-		length = STACK_LEN;
-		arr = new T[STACK_LEN];
+		length = STACK_LEN; // 길이 = STACK_LEN = 100
+		arr = new T[STACK_LEN]; // 배열 동적할당
 	}
-	ArrStack(int len) {
-		top = -1;
-		length = len;
-		arr = new T[len];
+	ArrStack(int len) { // 길이 따로 정하면
+		top = -1; // top 초기값 -1
+		length = len; // 지정한 길이로 길이 지정
+		arr = new T[len]; // 배열 동적할당
 	}
 	void Stack_Init() // 배열 스택 초기화
 	{
-		for (int i = 0; i < STACK_LEN; i++) {
-			arr[i] = 0;
+		for (int i = 0; i < STACK_LEN; i++) { // 배열 길이만큼
+			arr[i] = 0; // 0으로 초기화
 		}
 	}
 	void Stack_Push(T item) // 데이터 삽입
 	{
 		if (!Stack_IsFull()) { // 가득차 있지 않다면,
 
-			top++;
-			arr[top] = item;
+			top++; // top 인덱스 올리고
+			arr[top] = item; // top에 새로운 데이터 삽입
 		}
 		else {
-			printf("Stack is full!\n");
-			exit(1);
+			printf("Stack is full!\n"); // 가득차 있다면
+			exit(1); // 프로그램 종료
 		}
 	}
 	T Stack_Pop() // 데이터 인출
 	{
-		int idx;
-		if (Stack_IsEmpty()){ //비어있다면,
-			printf("Stack is empty!\n");
-			exit(1);
+		int idx; // 인덱스 선언
+		if (Stack_IsEmpty()){ // 비어있다면,
+			printf("Stack is empty!\n"); 
+			exit(1); // 프로그램 종료
 		}
-		else {
-			idx = top;
-			top--;
-			return arr[idx];
+		else { // 비어있지 않다면,
+			idx = top; // 인덱스에 현재 top 저장한 후
+			top--; // top 인덱스 1 빼기
+			return arr[idx]; // 빼기 전 top이었던 데이터 인출
 		}
 	}
 	T Stack_Peek() // 최상단 데이터 확인
 	{
-		return arr[top];
+		return arr[top]; // top 위치 데이터 return
 	}
 	bool Stack_IsEmpty() // 스택이 비어있는지 확인
 	{
-		return top == -1;
+		return top == -1; // top이 -1이면 True or False
 	}
 	bool Stack_IsFull() // 스택이 가득 차 있는지 확인
 	{
-		return top == length-1;
+		return top == length-1; // top이 길이 - 1인지 아닌지 True or False
 	}
 }; //Array Stack 클래스 선언
 
